5b55ba22ec7fab0c449b474caec29242a3b8dc1d
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-20 01:41:15 +0200

    taskk-2

--- task-1.js
+++ task-1.js
@@ -7,6 +7,7 @@ const logItems = function(array) {
   }
   return message;
 };
+
 logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
 
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
---
+++ task-2.js
@@ -0,0 +1,29 @@
+"use strict";
+let engravingPrice;
+const calculateEngravingPrice = function(message, pricePerWord) {
+  message = message.split(" ");
+  engravingPrice = pricePerWord * message.length;
+  return engravingPrice;
+};
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+);
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+);
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+);
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+);
