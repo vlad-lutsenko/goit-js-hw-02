6e24715567e4b3826b6e2c2ee0f7b1fdde86d3a9
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-20 01:26:29 +0200

    task-1

---
+++ task-1.js
@@ -0,0 +1,12 @@
+"use strict";
+let message;
+const logItems = function(array) {
+  for (let i = 0; i < array.length; i += 1) {
+    message = `${[i + 1]} - ${array[i]}`;
+    console.log(message);
+  }
+  return message;
+};
+logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
