fdb169677e1643ead9e5792b3fa9c072c9ee4ba9
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-20 16:03:54 +0200

    task-6

--- task-6.js
+++ task-6.js
@@ -3,21 +3,19 @@ let input;
 const numbers = [];
 let total = 0;
 
-const totalSum = input => {
-  do {
-    input = prompt("введите число!");
+do {
+  input = prompt("введите число!");
 
-    if (Number.isNaN(Number(input))) {
-      alert("Было введено не число, попробуйте еще раз");
-    } else {
-      numbers.push(Number(input));
-      console.log(numbers);
-    }
-  } while (input !== null);
-
-  for (let number of numbers) {
-    total += number;
+  if (Number.isNaN(Number(input))) {
+    alert("Было введено не число, попробуйте еще раз");
+  } else {
+    numbers.push(Number(input));
+    console.log(numbers);
   }
-  return total;
-};
-alert(`сумма чисел в массиве равна ${totalSum()}`);
+} while (input !== null);
+
+for (let number of numbers) {
+  total += number;
+}
+
+alert(`сумма чисел в массиве равна ${total}`);
