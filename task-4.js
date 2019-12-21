b28759e97ae6022cb0408cd8d2008bcc584574c9
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-21 03:07:20 +0200

    no excessive cls

--- task-4.js
+++ task-4.js
@@ -3,16 +3,10 @@ const formatString = function(string) {
   return string.length <= 40 ? string : `${string.slice(0, 40)}...`;
 };
 console.log(formatString("Curabitur ligula sapien, tincidunt non."));
-// console.log(formatString("Curabitur ligula sapien, tincidunt non.").length);
 
 console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
-console
-  .log
-  // formatString("Vestibulum facilisis, purus nec pulvinar iaculis.").length
-  ();
 
 console.log(formatString("Curabitur ligula sapien."));
-// console.log(formatString("Curabitur ligula sapien.").length);
 
 console.log(
   formatString(
