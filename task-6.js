907ffdf94424fc68e2fb4f591611baec54a2611a
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-21 02:20:27 +0200

    task-6 fixed

--- task-6.js
+++ task-6.js
@@ -6,17 +6,19 @@ let total = 0;
 do {
   input = prompt("введите число!");
 
-  if (Number.isNan(input)) {
+  if (Number.isNaN(Number(input))) {
     alert("Было введено не число, попробуйте еще раз");
-  } else {
+    continue;
+  }
+
+  if (input !== null) {
     numbers.push(Number(input));
-    console.log(numbers);
   }
 } while (input !== null);
 
-if (numbers.length > 0) {
+if (numbers.length >= 1) {
   for (let number of numbers) {
     total += number;
+    console.log(`сумма чисел в массиве равна ${total}`);
   }
 }
-console.log(`сумма чисел в массиве равна ${total}`);
